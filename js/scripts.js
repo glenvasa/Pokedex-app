53f448158d93941286fe6944c319e6b56c73d0b2
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-09-13 15:28:59 -0400

    Created 'for' loop
    loop writes the name and height of each Pokemon in the array and also checks to see if the height > 1.5. If height > 5, then special designation is written next to name and height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 let pokemonList = [];
 pokemonList = [
-  { name: 'Vulpix', height: 6, types: ['fire'] },
-  { name: 'Eevee', height: 3, types: ['normal'] },
-  { name: 'Arcanine', height: 19, types: ['fire'] },
+  { name: 'Vulpix',
+    height: 0.6,
+    types: ['fire']
+  },
+  { name: 'Eevee',
+    height: 0.3,
+    types: ['normal']
+  },
+  { name: 'Arcanine',
+    height: 1.9,
+    types: ['fire'] },
 ];
+
+
+for (let i=0; i < pokemonList.length; i++) {
+// sets threshold for big pokemon at > 1.5
+  if (pokemonList[i].height > 1.5)
+    {document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow! That's a big Pokemon! </p>");
+  }
+  else {document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") </p>");
+  }
+}
