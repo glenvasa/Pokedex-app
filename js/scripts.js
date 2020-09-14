ef5a0de4ec94be6b5631e054678863625d1fc57e
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-09-14 16:17:35 -0400

    Change - remove whitespace/clean up formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
 let pokemonList = [];
 pokemonList = [
-  { name: 'Vulpix',
-    height: 0.6,
-    types: ['fire']
-  },
-  { name: 'Eevee',
-    height: 0.3,
-    types: ['normal']
-  },
-  { name: 'Arcanine',
-    height: 1.9,
-    types: ['fire'] },
+  { name: 'Vulpix', height: 0.6, types: ['fire']},
+  { name: 'Eevee', height: 0.3, types: ['normal']},
+  { name: 'Arcanine',height: 1.9, types: ['fire']},
 ];
 
-
 for (let i=0; i < pokemonList.length; i++) {
-// sets threshold for big pokemon at > 1.5
-  if (pokemonList[i].height > 1.5)
-    {document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow! That's a big Pokemon! </p>");
-  }
-  else {document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") </p>");
+  // sets threshold for big pokemon at > 1.5
+  if (pokemonList[i].height > 1.5) {
+    document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow! That's a big Pokemon! </p>");
+} else {
+    document.write("<p> " + pokemonList[i].name + " (height: " + pokemonList[i].height + ") </p>");
   }
 }
