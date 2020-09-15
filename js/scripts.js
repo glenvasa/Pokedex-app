9a057bf26d4c88956c5e4e246cdab2c59cbdb183
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-09-15 14:07:50 -0400

    Change - Wrapped pokemonList array in an IIFE
    IIFE returns object with 2 public functions:
    
    getAll: returns all items
    add: adds a single item to the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
   { name: 'Vulpix', height: 0.6, types: ['fire']},
   { name: 'Eevee', height: 0.3, types: ['normal']},
-  { name: 'Arcanine',height: 1.9, types: ['fire']}
+  { name: 'Arcanine', height: 1.9, types: ['fire']}
 ];
 
-pokemonList.forEach(function(pokemon) {
+  function getAll () {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll : getAll,
+    add: add
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height < 1.5) {
   document.write('<p>name: ' + pokemon.name + '<br>height: ' + pokemon.height + '<br>types: ' + pokemon.types + '</p>');
+} else {
+  document.write('<p>name: ' + pokemon.name + '<br>height: ' + pokemon.height + '  (WOW! That\'s a big Pokemon!)' + '<br>types: ' + pokemon.types + '</p>');
+}
 });
